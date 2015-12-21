21abb90a85233d6b781a827adb902bbdb972ebd4
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2015-12-21 15:36:11 +0000

    fix example

--- examples/usertags/usertags.js
+++ examples/usertags/usertags.js
@@ -1,45 +1,6 @@
 (function(Backbone, Tags) {
   'use strict';
 
-<<<<<<< HEAD
-  console.log('USERTAGS')
-
-  var User = Backbone.Model.extend({
-    defaults: {
-      name: 'no name'
-    },
-    url: 'users'
-  });
-  User = User.extend(Tags.Extend);
-
-  var Users = Backbone.Collection.extend({
-    model: User,
-    url: 'users'
-  })
-
-  var edgar = new User({
-    id: '1',
-    name: 'Edgar',
-    tags: ['1', '2']
-  }, {
-    parse: true
-  });
-
-  var users = new Users(edgar);
-
-  edgar.on('change', function() {
-    console.log('CHANGE ON USER EDGAR', arguments)
-  });
-
-  users.get('1').detach('1')
-
-  console.log('LIB', Tags.Library.length)
-
-  //users.get('1').attach('1');
-
-  console.log('EDGAR', edgar);
-
-=======
   console.log(Tags);
 
   var User = Backbone.Model.extend({
@@ -66,6 +27,5 @@
   Tags.Library.at(0).fetch();
 
   console.log(Tags.Library);
->>>>>>> something
 
 })(Backbone, Tags);
